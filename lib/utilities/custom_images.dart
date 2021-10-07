44c953c6e6da425cd8210bc365b408c7b8bdeb75
askabdulhaseeb <askabdulhaseeb@gmail.com>
2021-10-08 00:22:34 +0500

    add images

---
+++ lib/utilities/custom_images.dart
@@ -0,0 +1,6 @@
+class CustomImages {
+  String emptyWishlist = 'assets/images/empty-wishlist.png';
+  String emptyCart = 'assets/images/emptycart.png';
+  String logo = 'assets/images/logo.png';
+  String icon = 'assets/images/icon.jpg';
+}
