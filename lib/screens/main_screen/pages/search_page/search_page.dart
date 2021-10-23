da9ab150a435da2ac3e8d22bf171718197a84b0d
askabdulhaseeb <askabdulhaseeb@gmail.com>
2021-10-23 12:57:57 +0500

    update search screen => no service found

--- lib/screens/main_screen/pages/search_page/search_page.dart
+++ lib/screens/main_screen/pages/search_page/search_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:volt_arena_app/models/product.dart';
+import 'package:volt_arena_app/providers/bottom_navigation_bar_provider.dart';
 import 'package:volt_arena_app/providers/products.dart';
 import 'package:volt_arena_app/screens/main_screen/pages/widgets/service_tile_widget.dart';
 import 'package:volt_arena_app/utilities/utilities.dart';
@@ -30,7 +31,6 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     final List<Product> _products = Provider.of<Products>(context).products;
-    final Size _size = MediaQuery.of(context).size;
     return Scaffold(
       body: Column(
         children: <Widget>[
@@ -50,12 +50,24 @@ class _SearchPageState extends State<SearchPage> {
                 ? Center(
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Icon(Icons.error_outline, size: 60, color: Colors.grey),
-                        SizedBox(height: 8),
-                        Text(
+                      children: <Widget>[
+                        const Icon(Icons.error_outline,
+                            size: 60, color: Colors.grey),
+                        const SizedBox(height: 8),
+                        const Text(
                           'No service found!!!',
                           style: TextStyle(color: Colors.grey, fontSize: 20),
+                        ),
+                        Consumer<BottomNavigationBarProvider>(
+                          builder: (
+                            BuildContext context,
+                            BottomNavigationBarProvider page,
+                            _,
+                          ) =>
+                              TextButton(
+                            onPressed: () => page.updateSelectedPage(0),
+                            child: const Text('SHOP NOW'),
+                          ),
                         ),
                       ],
                     ),
